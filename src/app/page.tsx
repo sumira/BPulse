c6449cb9ac5671ae7eac45771d948f1e8895e286
Sumira <sumiragp@gmail.com>
2024-12-25 10:57:36 +0530

    Add UI components for battery settings and integrate tabs for different battery types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import mqtt, { IClientOptions } from "mqtt";
 import { GaugeMeter } from "./Components/guage-meter";
+import { TabMenu } from "./Components/tab-menu";
 
 export default function Home() {
   const [connectionStatus, setConnectionStatus] = useState("Disconnected");
@@ -56,26 +57,28 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">MQTT Connection Status</h1>
-      <p className="mb-4">Status: {connectionStatus}</p>
-      <div className="mb-4">
-        <h2 className="text-xl font-bold">Messages:</h2>
-        {messages.map((message, index) => (
-          <div key={index} className="p-2 border-b">
-            {message}
-          </div>
-        ))}
-      </div>
-      <div>
-        <GaugeMeter
-          minValue={0}
-          maxValue={100}
-          value={value}
-          description={"Chart"}
-          title="Battery"
-        />
-      </div>
-    </main>
+    <>
+      <main className="p-4">
+        <h1 className="text-2xl font-bold mb-4">MQTT Connection Status</h1>
+        <p className="mb-4">Status: {connectionStatus}</p>
+        <div className="mb-4">
+          <h2 className="text-xl font-bold">Messages:</h2>
+          {messages.map((message, index) => (
+            <div key={index} className="p-2 border-b">
+              {message}
+            </div>
+          ))}
+        </div>
+        <div>
+          <GaugeMeter
+            minValue={0}
+            maxValue={100}
+            value={value}
+            description={"Chart"}
+            title="Battery"
+          />
+        </div>
+      </main>
+    </>
   );
 }

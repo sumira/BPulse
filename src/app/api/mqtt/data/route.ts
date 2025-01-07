cebaefc7a62678df3681cdf4ab360d533573f80e
Sumira <sumiragp@gmail.com>
2025-01-07 11:44:20 +0530

    Refactor GET function in MQTT data route to remove unused request parameter

--- src/app/api/mqtt/data/route.ts
+++ src/app/api/mqtt/data/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import {NextResponse } from 'next/server';
 import MQTTBackendService from '@/lib/mqtt';
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
   try {
     const mqtt = MQTTBackendService.getInstance();
     const temperature = mqtt.getTopicValue('bpulse/temperature');

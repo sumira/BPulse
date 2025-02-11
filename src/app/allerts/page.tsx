bd648d8a8ae5cf4a2b47f93bd48ad483d285fa8d
Sumira <sumiragp@gmail.com>
2025-02-11 06:35:13 +0530

    Refactor alert display logic to show a message when no alerts are present

--- src/app/allerts/page.tsx
+++ src/app/allerts/page.tsx
@@ -10,32 +10,38 @@ interface AlertData {
 }
 
 const alertData: AlertData[] = [
-  {
-    severity: "Critical",
-    title: "Overheating Detected",
-    description: "Battery temperature exceeded 45°C.",
-    timestamp: "2024-12-30, 14:35:00",
-  },
-  {
-    severity: "Medium",
-    title: "High Current Usage",
-    description: "Discharge current is nearing the safe threshold.",
-    timestamp: "2024-12-30, 13:15:00",
-  },
-  {
-    severity: "Low",
-    title: "Battery Health Good",
-    description: "Battery is operating within normal parameters.",
-    timestamp: "2024-12-30, 10:00:00",
-  },
+  // {
+  //   severity: "Critical",
+  //   title: "Overheating Detected",
+  //   description: "Battery temperature exceeded 45°C.",
+  //   timestamp: "2024-12-30, 14:35:00",
+  // },
+  // {
+  //   severity: "Medium",
+  //   title: "High Current Usage",
+  //   description: "Discharge current is nearing the safe threshold.",
+  //   timestamp: "2024-12-30, 13:15:00",
+  // },
+  // {
+  //   severity: "Low",
+  //   title: "Battery Health Good",
+  //   description: "Battery is operating within normal parameters.",
+  //   timestamp: "2024-12-30, 10:00:00",
+  // },
 ];
 
 export default function AllertsPage() {
   return (
     <div className="p-3">
-      {alertData.map((data, index) => (
-        <AlertCard key={index} data={data} />
-      ))}
+      {alertData.length > 0 ? (
+        alertData.map((data, index) => <AlertCard key={index} data={data} />)
+      ) : (
+        <div className="flex items-center justify-center h-screen  rounded-lg shadow-md bg-gray-50">
+          <p className="text-gray-500 text-4xl font-semibold">
+            No Alerts right now
+          </p>
+        </div>
+      )}
     </div>
   );
 }
